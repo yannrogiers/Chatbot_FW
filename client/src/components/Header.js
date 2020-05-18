8f87bfbb2047e94c63c028f5018838458944fe57
yann rogiers <yann.rogiers@student.ehb.be>
2020-05-18 22:03:52 +0200

    kleine styling update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+//juiste nav kleur: 00a99d
+
 const Header = () =>
     (
         <nav>
-            <div className="nav-wrapper">
-            <Link to={'/'} className="brand-logo">Logo</Link>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <div className="nav-wrapper indigo">
+            <Link to={'/'} className="brand-logo" style={{paddingLeft: "100px"}}>Logo</Link>
+            <ul id="nav-mobile" className="right hide-on-med-and-down" style={{paddingRight: "100px"}}>
                 <li><Link to={'/shop'}>Shop</Link></li>
                 <li><Link to={'/about'}>About us</Link></li>
             </ul>
