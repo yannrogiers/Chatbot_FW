b82b8e0e4d5cc3a369256989b95f42519a85b308
yann rogiers <yann.rogiers@student.ehb.be>
2020-05-29 17:07:36 +0200

    debugging

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
 
+if (process.env.NODE_ENV === 'production') {
+    //JS & CSS files
+    app.use(express.static('client/build'));
+
+    //Index.html for all page routes
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.use(express.json());
 app.use(cors());
 app.use(
@@ -34,16 +45,7 @@ app.use(bodyParser.json());
 require('./Routes/dialogFlowRoutes')(app);
 require('./Routes/fulFillmentRoutes')(app);
 
-if (process.env.NODE_ENV === 'production') {
-    //JS & CSS files
-    app.use(express.static('client/build'));
 
-    //Index.html for all page routes
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
