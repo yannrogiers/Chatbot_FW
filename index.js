25e43b029a05f48f9ff65e5c0d1e376ad5519d24
yann rogiers <yann.rogiers@student.ehb.be>
2020-06-05 20:48:08 +0200

    removed test from db

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
 
     //Index.html for all page routes
     const path = require('path');
-    app.get('/^\/(?!api).*', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
