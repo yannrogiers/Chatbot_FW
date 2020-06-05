498acc760e007e798852530eb5848d2a342e9702
yann rogiers <yann.rogiers@student.ehb.be>
2020-06-05 18:04:23 +0200

    debug

--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -6,17 +6,19 @@ const {isAdmin, isAuth} = require('../util');
 
 
 router.get('/', async (req, res) => {
+    
     const category = req.query.category ? {category: req.query.category} : {};
     const searchKeyword = req.query.searchKeyword ?{
-        /*name:{
+        name:{
             $regex: req.query.searchKeyword,
             $options: 'i'
-        }*/
+        }
     } : {}
     const sortOrder = req.query.sortOrder ? (req.query.sortOrder === 'lowest' ? {price: 1}:{price: -1})
     :
     {_id: -1}
     const products = await Product.find({...category, ...searchKeyword}).sort(sortOrder);
+    console.log(products)
     res.send(products);
 });
 
