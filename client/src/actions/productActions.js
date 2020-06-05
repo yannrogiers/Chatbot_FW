61766ba355387c9ee081bfa41710dfc028e38ce6
yann rogiers <yann.rogiers@student.ehb.be>
2020-06-05 20:15:54 +0200

    debug2

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -5,8 +5,7 @@ import Axios from "axios";
 const listProducts = (category ='', searchKeyword='', sortOrder='') => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST });
-        const { data } = await axios.get("/api/products/searchProducts?category=" + category +
-        "&searchKeyword=" + searchKeyword + "&sortOrder=" + sortOrder);
+        const { data } = await axios.get("/api/products/searchProducts")
         dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
     } catch (error) {
         dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message })
