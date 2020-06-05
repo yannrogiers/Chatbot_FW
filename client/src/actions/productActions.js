4ede489c8fe7e60c071810c37d7dd917143c0fd9
yann rogiers <yann.rogiers@student.ehb.be>
2020-06-05 18:22:25 +0200

    asssss

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -1,11 +1,11 @@
 import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_SAVE_REQUEST, PRODUCT_SAVE_SUCCESS, PRODUCT_SAVE_FAIL, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_FAIL } from "../constants/productConstants"
-import axios from 'axios'
+
 import Axios from "axios";
 
 const listProducts = (category ='', searchKeyword='', sortOrder='') => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST });
-        const { data } = await axios.get("/api/products?category=" + category + 
+        const { data } = await Axios.get("/api/products?category=" + category + 
         "&searchKeyword=" + searchKeyword 
         + "&sortOrder=" + sortOrder);
         dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
@@ -20,14 +20,14 @@ const saveProduct = (product) => async (dispatch, getState) => {
         dispatch({ type: PRODUCT_SAVE_REQUEST, payload: product });
         const { userSignin: { userInfo } } = getState();
         if (!product._id) {
-            const { data } = await Axios.post('/api/products', product, {
+            const { data } = await Axios.post('https://infinite-castle-02083.herokuapp.com/api/products', product, {
                 headers: {
                     'Authorization': 'Bearer ' + userInfo.token
                 }
             });
             dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
         } else {
-            const { data } = await Axios.put('/api/products/' + product._id, product, {
+            const { data } = await Axios.put('https://infinite-castle-02083.herokuapp.com/api/products/' + product._id, product, {
                 headers: {
                     'Authorization': 'Bearer ' + userInfo.token
                 }
@@ -45,7 +45,7 @@ const detailsProduct = (productId) => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
         //Get specific product from server
-        const { data } = await axios.get('/api/products/' + productId);
+        const { data } = await Axios.get('/api/products/' + productId);
         dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
     } catch (error) {
         dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.message });
@@ -57,7 +57,7 @@ const deleteProduct = (productId) => async (dispatch, getState) => {
         const {userSignin : {userInfo}} = getState();
         dispatch({ type: PRODUCT_DELETE_REQUEST, payload: productId });
         //Get specific product from server
-        const { data } = await axios.delete('/api/products/' + productId, {
+        const { data } = await Axios.delete('/api/products/' + productId, {
             headers: {
                 Authorization: 'Bearer ' + userInfo.token
             }
